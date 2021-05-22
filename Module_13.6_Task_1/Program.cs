4c185b41db962554cbc04c255d293465ad3eb561
Andrew <konoplyanik0504@gmail.com>
2021-05-22 12:29:13 +0300

    Add project files.

---
+++ Module_13.6_Task_1/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+
+namespace Module_13._6_Task_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = @"C:\Users\Lenovo\source\repos\Module_13.6_Task_1\cdev_Text.txt";
+
+            var textList1 = new List<string>();
+            var textList2 = new LinkedList<string>();
+
+            var stopWatch1 = Stopwatch.StartNew();
+            try
+            {
+                using (var sr = new StreamReader(path))
+                {
+                    var text = sr.ReadToEnd();
+                    foreach (var item in text.Split(' ', '!', '\''))
+                    {
+                        textList1.Add(item);
+                    }
+                }
+            }
+
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            Console.WriteLine($"Производительность вставки в List<T>: {stopWatch1.Elapsed.TotalMilliseconds}");
+
+            Console.WriteLine();
+
+            var stopWatch2 = Stopwatch.StartNew();
+            try
+            {
+                using (var sr = new StreamReader(path))
+                {
+                    var text = sr.ReadToEnd();
+
+                    foreach (var item in text.Split(' ', '!', '\''))
+                    {
+                        textList2.AddFirst(item);
+                    }
+                }
+            }
+
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            Console.WriteLine($"Производительность вставки в LinkedList<T>: {stopWatch2.Elapsed.TotalMilliseconds}");
+
+            Console.ReadLine();
+        }
+    }
+}
